0700c40ba9bce23e4855a0a5b023856b97859d13
choyeaeun <silbia629@gmail.com>
2019-10-01 11:23:45 +0900

    Done
    AutoLayout
    image, Btn image set
    like Button

--- 25th_1st_seminar/25th_1st_seminar/Sources/ViewController.swift
+++ 25th_1st_seminar/25th_1st_seminar/Sources/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var profileIMG: UIImageView!
+    @IBOutlet var profileID: UILabel!
+    @IBOutlet var feedIMG: UIImageView!
+    @IBOutlet var likeBtn: UIButton!
+    @IBOutlet var likeID: UILabel!
+    @IBOutlet var likeLB: UILabel!
+    
+    var like : Bool = false
+    let heart = UIImage(systemName: "heart")
+    let heartfill = UIImage(systemName: "heart.fill")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        profileIMG.image = #imageLiteral(resourceName: "malong")
+        profileIMG.layer.cornerRadius = profileIMG.frame.height/2
+        profileIMG.clipsToBounds = true
+        profileID.text = "yeanny0_0"
+        feedIMG.image = #imageLiteral(resourceName: "insta_image")
+        
+        
     }
 
-
+    @IBAction func likeBtn(_ sender: Any) {
+        if(!like){
+            likeBtn.setImage(heart, for: .normal)
+            likeBtn.tintColor = .black
+            likeID.isHidden = true
+            likeLB.isHidden = true
+            like = true
+        }else{
+            likeBtn.setImage(heartfill, for: .normal)
+            likeBtn.tintColor = .red
+            likeID.isHidden = false
+            likeID.text = "yeanny0_0"
+            likeLB.isHidden = false
+            like = false
+        }
+    }
+    
 }
 
