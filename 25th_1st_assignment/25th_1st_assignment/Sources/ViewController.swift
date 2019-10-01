41f4675e0549bacd2802b5093b2039dd331b5de1
choyeaeun <silbia629@gmail.com>
2019-10-01 11:59:04 +0900

    Rename project
    to assignment

--- 25th_1st_assignment/25th_1st_assignment/Sources/ViewController.swift
+++ 25th_1st_assignment/25th_1st_assignment/Sources/ViewController.swift
@@ -17,13 +17,14 @@ class ViewController: UIViewController {
     @IBOutlet var likeID: UILabel!
     @IBOutlet var likeLB: UILabel!
     
-    var like : Bool = false
+    //좋아요 버튼 : 시스템 이미지 설정
     let heart = UIImage(systemName: "heart")
     let heartfill = UIImage(systemName: "heart.fill")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //프로필 이미지, 아이디, 피드 이미지 설정
         profileIMG.image = #imageLiteral(resourceName: "malong")
         profileIMG.layer.cornerRadius = profileIMG.frame.height/2
         profileIMG.clipsToBounds = true
@@ -32,21 +33,23 @@ class ViewController: UIViewController {
         
         
     }
-
+    //좋아요 버튼 액션
     @IBAction func likeBtn(_ sender: Any) {
-        if(!like){
+        
+        let btn = sender as! UIButton
+        btn.isSelected = !btn.isSelected
+        
+        if(btn.isSelected){
             likeBtn.setImage(heart, for: .normal)
             likeBtn.tintColor = .black
             likeID.isHidden = true
             likeLB.isHidden = true
-            like = true
         }else{
             likeBtn.setImage(heartfill, for: .normal)
             likeBtn.tintColor = .red
             likeID.isHidden = false
             likeID.text = "yeanny0_0"
             likeLB.isHidden = false
-            like = false
         }
     }
     
